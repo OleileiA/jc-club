d7e680d75c876c00559bb19cef611385713882f4
OleileiA <972260032@qq.com>
2024-08-19 17:59:16 +0800

    引入sl4j2日志，排除重复日志依赖

--- jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/jingdianjichi/subject/controller/SubjectCategoryController.java
+++ jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/jingdianjichi/subject/controller/SubjectCategoryController.java
@@ -1,10 +1,12 @@
 package com.jingdianjichi.subject.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.jingdianjichi.subject.common.entity.Result;
 import com.jingdianjichi.subject.convert.SubjectCategoryDTOConverter;
 import com.jingdianjichi.subject.domain.entity.SubjectCategoryBO;
 import com.jingdianjichi.subject.domain.service.SubjectCategoryDomainService;
 import com.jingdianjichi.subject.dto.SubjectCategoryDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import javax.annotation.Resource;
 /**
  * 刷题分类
  */
+@Slf4j
 @RestController
 @RequestMapping("/subject/category")
 public class SubjectCategoryController {
@@ -23,9 +26,14 @@ public class SubjectCategoryController {
     private SubjectCategoryDomainService subjectCategoryDomainService;
 
     @PostMapping("/add")
-    public Result<Boolean> test(@RequestBody SubjectCategoryDTO subjectCategoryDTO) {
+    public Result<Boolean> add(@RequestBody SubjectCategoryDTO dto) {
         try {
-            SubjectCategoryBO subjectCategoryBO = SubjectCategoryDTOConverter.INSTANCE.convertDTOToBoCategory(subjectCategoryDTO);
+            // log.isInfoEnabled() 用于检查是否需要执行 log.info() 方法。这样做的好处是，只有在 INFO 级别的日志启用时，才会进行消息的生成和拼接
+            // 从而避免了在日志级别不启用的情况下执行不必要的计算。
+            if (log.isInfoEnabled()) {
+                log.info("SubjectCategoryController.add.dto: {}", JSON.toJSONString(dto));
+            }
+            SubjectCategoryBO subjectCategoryBO = SubjectCategoryDTOConverter.INSTANCE.convertDTOToBoCategory(dto);
             subjectCategoryDomainService.add(subjectCategoryBO);
             return Result.ok(true);
         } catch (Exception e) {
--- jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
+++ jc-club-subject/jc-club-domain/src/main/java/com/jingdianjichi/subject/domain/service/impl/SubjectCategoryDomainServiceImpl.java
@@ -1,22 +1,30 @@
 package com.jingdianjichi.subject.domain.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.jingdianjichi.subject.domain.convert.SubjectCategoryConverter;
 import com.jingdianjichi.subject.domain.entity.SubjectCategoryBO;
 import com.jingdianjichi.subject.domain.service.SubjectCategoryDomainService;
 import com.jingdianjichi.subject.infra.basic.entity.SubjectCategory;
 import com.jingdianjichi.subject.infra.basic.service.SubjectCategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.omg.CORBA.PRIVATE_MEMBER;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+@Slf4j
+@Component
 public class SubjectCategoryDomainServiceImpl implements SubjectCategoryDomainService {
 
     @Resource
     private SubjectCategoryService subjectCategoryService;
 
     @Override
-    public void add(SubjectCategoryBO subjectCategoryBO) {
-        SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE.convertBOToCategory(subjectCategoryBO);
+    public void add(SubjectCategoryBO bo) {
+        if (log.isInfoEnabled()) {
+            log.info("SubjectCategoryDomainService.add.bo: {}", JSON.toJSONString(bo));
+        }
+        SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE.convertBOToCategory(bo);
         subjectCategoryService.insert(subjectCategory);
     }
 }
