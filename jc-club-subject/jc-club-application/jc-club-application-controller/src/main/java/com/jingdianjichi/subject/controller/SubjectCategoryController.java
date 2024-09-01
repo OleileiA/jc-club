af4ddc72ddc22c3765a070b6f2fb025b8e9b384b
OleileiA <972260032@qq.com>
2024-09-02 00:10:26 +0800

    增加参数校验

--- jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/jingdianjichi/subject/controller/SubjectCategoryController.java
+++ jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/jingdianjichi/subject/controller/SubjectCategoryController.java
@@ -1,6 +1,7 @@
 package com.jingdianjichi.subject.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.base.Preconditions;
 import com.jingdianjichi.subject.common.entity.Result;
 import com.jingdianjichi.subject.convert.SubjectCategoryDTOConverter;
 import com.jingdianjichi.subject.domain.entity.SubjectCategoryBO;
@@ -27,17 +28,24 @@ public class SubjectCategoryController {
 
     @PostMapping("/add")
     public Result<Boolean> add(@RequestBody SubjectCategoryDTO dto) {
+
         try {
             // log.isInfoEnabled() 用于检查是否需要执行 log.info() 方法。这样做的好处是，只有在 INFO 级别的日志启用时，才会进行消息的生成和拼接
             // 从而避免了在日志级别不启用的情况下执行不必要的计算。
             if (log.isInfoEnabled()) {
                 log.info("SubjectCategoryController.add.dto: {}", JSON.toJSONString(dto));
             }
+
+            // 参数的校验
+            Preconditions.checkNotNull(dto.getCategoryName(), "分类名字不能为空");
+            Preconditions.checkNotNull(dto.getCategoryType(), "分类类型不能为空");
+
             SubjectCategoryBO subjectCategoryBO = SubjectCategoryDTOConverter.INSTANCE.convertDTOToBoCategory(dto);
             subjectCategoryDomainService.add(subjectCategoryBO);
             return Result.ok(true);
         } catch (Exception e) {
-            return Result.fail();
+            log.error("SubjectCategoryController.add.error:{}", e.getMessage(), e);
+            return Result.fail(e.getMessage());
         }
     }
 }
